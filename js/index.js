1d0da124fac69c53d5a29fbd5bfb867e53c36d90
Jesus Soto <118070829+Jesus-Soto-Dev@users.noreply.github.com>
2025-09-29 13:29:30 +0200

    avance 29-09

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const Datos = {"Grand Theft Auto VI": "Watch Trailer 2 Now", "Grand Theft Auto Online":"Money Fronts Now Available","Grand Theft Auto V":"Experience the Best Version of GTAV and GTA Online on PC with GTAV Enhanced","Red Dead Online":"Strange Tales of the West"};
+
+const DatosImg = {"First slide":"Grand Theft Auto VI", "Second slide":"Grand Theft Auto Online","Third slide":"Grand Theft Auto V","Fourth slide":"Red Dead Online"};
+
+
+generarItem(){
+
+}
